2995d311d89a49c54cc704b25a9e8773e98dc149
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-09-27 13:21:52 +0100

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ExelFileRead';
+  file:File;
+  arrayBuffer: any;
+  filelist: any;
+
+  addfile(event)
+  {
+  this.file= event.target.files[0];
+  let fileReader = new FileReader();
+  fileReader.readAsArrayBuffer(this.file);
+  fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      var data = new Uint8Array(this.arrayBuffer);
+      var arr = new Array();
+      for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join("");
+      var workbook = XLSX.read(bstr, {type:"binary"});
+      var first_sheet_name = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+        var arraylist = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+            this.filelist = [];
+            console.log(this.filelist)
+
+  }
+}  
 }
